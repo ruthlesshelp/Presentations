a1d6df4310d3f47f97982928f07b3abddadbedb1
stephen.ritchie <stephen.ritchie@excella.com>
2012-05-12 12:39:55 -0400

    Corrected the RoundhouseSilent parameter.

--- RoundhousE/Lender.Slos.Deploy.cmd
+++ RoundhousE/Lender.Slos.Deploy.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "database.deploy.msbuild" /p:DBServer=(local)\SQLExpress;DBName=Lender.Slos;Silent=False
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "database.deploy.msbuild" /p:DBServer=(local)\SQLExpress;DBName=Lender.Slos;RoundhouseSilent=False
 
 if "%1" NEQ "noprompt" PAUSE
