c7492ad3a37963816c3568d1b396f80d1c779a8c
stephen.ritchie <stephen.ritchie@excella.com>
2012-09-03 13:59:28 -0400

    Added the introduction example to explain automated testing motivation.

---
+++ AutoTesting/_Start/Tests.Unit.Lender.Slos.Financial/ProductionSettingsTests.cs
@@ -0,0 +1,35 @@
+﻿using Lender.Slos.Utilities.Configuration;
+
+using NUnit.Framework;
+
+namespace Tests.Unit.Lender.Slos.Financial
+{
+    public class ProductionSettingsTests
+    {
+        [Test]
+        public void GetStartMonth_Always_Expect12()
+        {
+            // Arrange
+            var classUnderTest = new ProductionSettings();
+
+            // Act
+            var result = classUnderTest.StartMonth;
+
+            // Assert
+            Assert.AreEqual(12, result);
+        }
+
+        [Test]
+        public void GetStartDay_Always_Expect1()
+        {
+            // Arrange
+            var classUnderTest = new ProductionSettings();
+
+            // Act
+            var result = classUnderTest.StartDay;
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+    }
+}
